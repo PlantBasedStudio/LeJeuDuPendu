f8365ef5a543375a9b991c27a961d103d2194c54
Damien <ultrarushgame@gmail.com>
2023-06-07 14:23:03 +0200

    mise a jour des fonctions principales

--- script.js
+++ script.js
@@ -2,51 +2,71 @@
 
 
 // ---- VARIABLES ----
-const WordsToFind = ["epistemology", "direction", "object", "push", "pull", "prototype", "fetch", "database"];;
+const WordsToFind = ["epistemology", "direction", "object", "push", "pull", "prototype", "fetch", "database", "display", "element", "console", "content", "style", "event", "index", "script", "practice", "modal", "learn", "teach", "code", "langage", "internet"];;
 let theWord = "";
+let hideWord;
 const elementWordToFind = document.getElementById("wordToFind");
-const attempsNumber = 7; // A afficher
+let attempsNumber = 7; // A afficher
+const attempsElementDisplay = document.getElementById("attempts");
 const keys = document.querySelectorAll(".key"); // permet de récupérer toutes les touches du jeu.
 const startGameButton = document.getElementById("startGameButton");
 let gameStarted = false;
 
 
-// ---- FONCTIONS ----
-// function startGame()
+
+//! ---- FONCTIONS ----
 function startGame() {
     startGameButton.style.display = "none";
     theWord = WordsToFind[Math.floor(Math.random() * WordsToFind.length)];
+    
     console.log(theWord);
     gameStarted = true;
     fractionTheWord();
 }
 
-// Function fractionner le mot en valeurs pour pouvoir les show + show le mot dans le HTML
 function fractionTheWord(){
-    elementWordToFind.textContent = "";
+    hideWord = theWord.split(" ");
     for (letters in theWord){
-        elementWordToFind.textContent += "*";
+        hideWord[letters] = "*";
     }
-
+    elementWordToFind.textContent = hideWord.join("");
 }
 
-// function readKey()
 function readKey(key) {
-    if (gameStarted){
+    if (gameStarted && attempsNumber > 0){
         console.log(key);
         searchCorrespondanceBeweenKeyAndWord(key);
     }
+    else if (attempsNumber <= 0)
+    {
+        console.log("Tu ne peux plus jouer");
+    }
 }
 
-// function searchCorrespondanceBeweenKeyAndWord()
 function searchCorrespondanceBeweenKeyAndWord(key) {
+    let count = 0;
     for (letters in theWord){
         if (theWord[letters] === key){
-            console.log("déjà t'as trouvé une lettre mon grand");
-            let newText = theWord.split(" ").pop(key);
-            elementWordToFind.textContent = newText;
+            actualisedWord(key);
+            count += 1;
         }
     }
+    // Le count sert a connaître s'il y a eu une correspondance ou non pour retirer un essai (uniquement en cas de nouvelle lettre car on laisse le joueur donner une lettre qu'il a déjà utiliser au clavier)
+    if (count === 0){
+        attempsNumber -= 1;
+        attempsElementDisplay.textContent = `${attempsNumber} trials left`;
+    }
+    
+}
+
+function actualisedWord(key) {
+    for (let i = 0; i < theWord.length; i++) {
+        if (theWord[i] === key) {
+            hideWord[i] = key;
+            
+        }
+    }
+    elementWordToFind.textContent = hideWord.join("");
 }
 
 
@@ -57,14 +77,25 @@ function searchCorrespondanceBeweenKeyAndWord(key) {
 // Option recommencer (fenêtre modale)
 
 
-//---- Events ----
+//!---- Events ----
 startGameButton.addEventListener("click", startGame);
 
+// Ajoute l'event de lecture du charactère si on clique sur le bouton correspondant.
 for (buttons of keys){
     buttons.addEventListener("click", function(){
         readKey(this.textContent);
     });
 
 }
+
+// Permet de détecter si une touche du clavier est pressée et de lire l'action si c'est bien un caractère.
+document.addEventListener("keydown", (event) =>{
+    if (event.key.search(/[^a-zA-Z]+/) == -1){
+        readKey(event.key);
+    }
+});
+
+
+
 // Option, afficher les lettres restantes en disabled les touches déjà préssées.
 // Afficher l'avancement du pendu entre chaque tentative.
